4f1d5c6df1ba41640da4b39edd3d41ce7e415379
Marcus Villarreal <mvill113@stu.southtexascollege.edu>
2021-06-11 07:45:40 -0500

    Assignment 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
